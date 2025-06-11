bee487c740b8eb8a217fe1a3c983576d35fbdb07
Jeevan <jeevanv.professional@gmail.com>
2025-06-12 00:12:50 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.31%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -2,30 +2,29 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int low = 0, high = matrix.size() - 1;
-
         while (low <= high) {
-            int mid = low + (high - low) / 2;
-
+            int mid = low + (high - low) / 2; // this will point to the mid'th vector
+            // if the target is in this current array , then apply bs here
+            int curr_start = 0, curr_end = matrix[mid].size() - 1;
             if (matrix[mid][0] <= target && target <= matrix[mid].back()) {
-                int curr_start = 0, curr_end = matrix[mid].size() - 1;
+                vector<int> curr_vec = matrix[mid];
                 while (curr_start <= curr_end) {
                     int curr_mid = curr_start + (curr_end - curr_start) / 2;
-                    if (matrix[mid][curr_mid] == target) {
+                    if (curr_vec[curr_mid] == target) {
                         return true;
-                    } else if (matrix[mid][curr_mid] < target) {
+                    } else if (curr_vec[curr_mid] < target) {
                         curr_start = curr_mid + 1;
                     } else {
                         curr_end = curr_mid - 1;
                     }
                 }
-                return false;
+                return false; // we searched the only possible row, target not found
             } else if (matrix[mid][0] > target) {
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
-
-        return false;
+        return false; // no valid row found
     }
 };
