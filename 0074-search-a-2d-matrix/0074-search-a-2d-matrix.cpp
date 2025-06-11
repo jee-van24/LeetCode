cc08a0196024b8bd8d8120b6e1be4053604f3132
Jeevan <jeevanv.professional@gmail.com>
2025-06-12 00:09:28 +0530

    Time: 0 ms (100%), Space: 13.2 MB (75.91%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        bool flag=false;
-        for(auto row: matrix){
-            for(auto val: row){
-                if(val==target){
-                    flag=true;
+        int low = 0, high = matrix.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (matrix[mid][0] <= target && target <= matrix[mid].back()) {
+                int curr_start = 0, curr_end = matrix[mid].size() - 1;
+                while (curr_start <= curr_end) {
+                    int curr_mid = curr_start + (curr_end - curr_start) / 2;
+                    if (matrix[mid][curr_mid] == target) {
+                        return true;
+                    } else if (matrix[mid][curr_mid] < target) {
+                        curr_start = curr_mid + 1;
+                    } else {
+                        curr_end = curr_mid - 1;
+                    }
                 }
+                return false;
+            } else if (matrix[mid][0] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
-        return flag;
-    }    
+
+        return false;
+    }
 };
