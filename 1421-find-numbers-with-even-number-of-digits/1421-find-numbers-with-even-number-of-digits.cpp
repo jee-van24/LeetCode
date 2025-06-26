4446732965ae1df7ab1ddac2d9190f4bf7ddd974
Jeevan <jeevanv.professional@gmail.com>
2025-06-26 23:42:21 +0530

    Time: 0 ms (100%), Space: 14.6 MB (12.71%) - LeetHub

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.cpp
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     int findNumbers(vector<int>& nums) {
-        vector<string>str;
-        int count=0;
-        for(auto num:nums){
-            string temp=to_string(num);
-            str.push_back(temp);
-        }
-        for(auto num:str){
-            if(num.size()%2==0){
-                count++;
-            }
+        int count = 0;
+        for (int i : nums) {
+            string s = to_string(i);
+            if (s.size() % 2 == 0) count++;
         }
         return count;
     }
