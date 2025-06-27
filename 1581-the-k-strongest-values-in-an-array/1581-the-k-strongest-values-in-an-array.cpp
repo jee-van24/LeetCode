784669431c202a5e0c10e1850948c5911b83440e
Jeevan <jeevanv.professional@gmail.com>
2025-06-27 15:55:44 +0530

    Time: 35 ms (70.54%), Space: 87.3 MB (39.53%) - LeetHub

--- 1581-the-k-strongest-values-in-an-array/1581-the-k-strongest-values-in-an-array.cpp
+++ 1581-the-k-strongest-values-in-an-array/1581-the-k-strongest-values-in-an-array.cpp
@@ -6,7 +6,7 @@ public:
         int count=0;
         int left=0,right=arr.size()-1,n=arr.size();
         int mid= arr[(n-1)/2];
-        while(count!=k){
+        while(count!=k&&left<=right){
             int left_strength=fabs(arr[left]-mid);
             int right_strength=fabs(arr[right]-mid);
             if(left_strength>right_strength||(left_strength==right_strength)&&arr[left]>arr[right]){
