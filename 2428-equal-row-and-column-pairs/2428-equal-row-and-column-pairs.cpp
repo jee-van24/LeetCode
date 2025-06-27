595f40af8b6e3554441269a804b193d3510caf53
Jeevan <jeevanv.professional@gmail.com>
2025-06-27 13:56:27 +0530

    Time: 23 ms (50.45%), Space: 31.2 MB (76.51%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
+        map<vector<int>, int> freq;
         int count = 0;
-        int n = grid.size();
 
         for (auto& row : grid) {
-            for (int col = 0; col < n; col++) {
-                bool isEqual = true;
-                for (int j = 0; j < n; j++) {
-                    if (row[j] != grid[j][col]) {
-                        isEqual = false;
-                        break;
-                    }
-                }
-                if (isEqual) {
-                    count++;
-                }
+            freq[row]++;
+        }
+
+        int n = grid.size();
+        for (int i = 0; i < n; i++) {
+            vector<int> col(n); 
+            for (int j = 0; j < n; j++) {
+                col[j] = grid[j][i]; 
+            }
+            if (freq.find(col) != freq.end()) {
+                count += freq[col];
             }
         }
 
