a3999657349db52b740cd6bf6f86f0cf302a2f98
Jeevan <jeevanv.professional@gmail.com>
2025-07-03 18:26:00 +0530

    Time: 0 ms (100%), Space: 8.5 MB (83.51%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     char kthCharacter(int k) {
-        string word="a";
-        int i=1;
-        while(word.size()<k){
-            string temp=word;
-            for(int j=0;j<temp.size();j++){
-                char curr=temp[j];
-                char next=(temp[j] - 'a' + 1) % 26 + 'a';
-                word+=next;
+        string word = "a";
+        while (word.size() < k) {
+            size_t oldSize = word.size();
+            for (size_t i = 0; i < oldSize && word.size() < k; ++i) {
+                char next = (word[i] - 'a' + 1) % 26 + 'a';
+                word += next;
             }
-            i++;
         }
-        return word[k-1];//since i will be equal to k when the loop ends
+        return word[k - 1];
     }
 };
