2ae478ed7e62524f223beca5e38ee2cc3940bf12
Jeevan <jeevanv.professional@gmail.com>
2025-07-06 19:13:50 +0530

    Time: 0 ms (100%), Space: 8.3 MB (95.71%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        string res=s;
-        for(int i=0;i<res.size();i++){
-            if(i>0&&isdigit(res[i])){
-                res.erase(i-1,2); 
-                i-=2;  
+        string res="";
+        for(int i=0;i<s.size();i++){
+            if(!res.empty()&&isdigit(s[i])){
+                res.pop_back();
+            }else{
+                res+=s[i];
             }
         }
         return res;
