bc027c9a37443415e13ab60aa6c8d79644cfa57e
Jeevan <jeevanv.professional@gmail.com>
2025-07-10 21:55:54 +0530

    Time: 0 ms (100%), Space: 18 MB (52.75%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        vector<pair<int,int>>check;
-        vector<int>res;
-        unordered_set<int>seen;
-        for(int i=0;i<nums.size();i++){
-            int curr=nums[i];
-            if(seen.find(curr)!=seen.end()){
-                continue;
-            }
-            int freq=0;
-            for(int j=0;j<nums.size();j++){
-                if(nums[j]==curr)freq++;
-            }
-            check.push_back({freq,curr});
-            seen.insert(curr);
+        unordered_map<int, int> freqMap;
+        vector<pair<int, int>> check;
+        vector<int> res;
+        for (int num : nums) {
+            freqMap[num]++;
         }
-        sort(check.begin(),check.end());
-            for(int i=check.size()-1;i>=0;i--){
-                if(res.size()>=k)break;
-                res.push_back(check[i].second);
-            }
-            return res;
+        for (auto& it : freqMap) {
+            check.push_back({it.second, it.first});
+        }
+        sort(check.begin(), check.end());
+        for (int i = check.size() - 1; i >= 0; i--) {
+            if (res.size() == k) break;
+            res.push_back(check[i].second);
+        }
+
+        return res;
     }
 };
