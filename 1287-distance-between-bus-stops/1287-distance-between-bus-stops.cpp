e0348415fc5551b3ed9fca823963a8d5bb160bb3
Jeevan <jeevanv.professional@gmail.com>
2025-07-15 19:02:05 +0530

    Time: 3 ms (59.88%), Space: 12.5 MB (80.66%) - LeetHub

--- 1287-distance-between-bus-stops/1287-distance-between-bus-stops.cpp
+++ 1287-distance-between-bus-stops/1287-distance-between-bus-stops.cpp
@@ -3,12 +3,13 @@ public:
     int distanceBetweenBusStops(vector<int>& distance, int start, int destination) {
         int d1=0,d2=0;
         if(start>destination)swap(start,destination);
-        for(int i=0;i<destination;i++){
-            if(i>=start&&start<destination){
-                d1+=distance[i];
-            }
+        for(int i=start;i<destination;i++){
+            d1+=distance[i];
         }
-        int total=accumulate(distance.begin(),distance.end(),0);
+        int total=0;
+        for(auto num:distance){
+            total+=num;
+        }
         d2=total-d1;
         return min(d1,d2);
     }
