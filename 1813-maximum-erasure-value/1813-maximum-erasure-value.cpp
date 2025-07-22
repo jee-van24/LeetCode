76841302737a3bfcd8954e0ee712607df9b3a74f
Jeevan <jeevanv.professional@gmail.com>
2025-07-22 14:31:28 +0530

    Time: 1004 ms (5.01%), Space: 93.3 MB (97.85%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int maximumUniqueSubarray(vector<int>&nums){
-        vector<int>seen;
-        int res=0;
-        for(int right=0;right<nums.size();right++){
-            int curr=nums[right];
-            if(find(seen.begin(),seen.end(),curr)==seen.end()){
-                seen.push_back(curr);
-            }else{
-                int sum=accumulate(seen.begin(),seen.end(),0);
-                res=max(res,sum);
-                while(find(seen.begin(),seen.end(),curr)!=seen.end()){
+    int maximumUniqueSubarray(vector<int>& nums) {
+        vector<int> seen;
+        int res = 0, curr_sum = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+            int curr = nums[right];
+            if (find(seen.begin(), seen.end(), curr) != seen.end()) {
+                while (find(seen.begin(), seen.end(), curr) != seen.end()) {
+                    curr_sum -= seen.front();
                     seen.erase(seen.begin());
                 }
-                seen.push_back(curr);
-
             }
-       }
-       int sum=accumulate(seen.begin(),seen.end(),0);
-       res=max(res,sum);
-       return res; 
+
+            seen.push_back(curr);
+            curr_sum += curr;
+            res = max(res, curr_sum);
+        }
+
+        return res;
     }
 };
