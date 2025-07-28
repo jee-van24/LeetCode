8259eec52e65226b087d0e4e9c1c9a56184dd876
Jeevan <jeevanv.professional@gmail.com>
2025-07-28 21:12:31 +0530

    Time: 10 ms (5.81%), Space: 22.3 MB (6.64%) - LeetHub

--- 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/2191-minimum-number-of-food-buckets-to-feed-the-hamsters.cpp
+++ 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/2191-minimum-number-of-food-buckets-to-feed-the-hamsters.cpp
@@ -1,26 +1,52 @@
 class Solution {
 public:
     int minimumBuckets(string hamsters) {
-        int count=0;
-        int n=hamsters.size();
-        for(int i=0;i<hamsters.size();i++){
-            if(hamsters[i]=='H'){
-                if(i>0&&hamsters[i-1]=='1'){
-                    continue;
-                }
-                else if(i+1<n&&hamsters[i+1]=='.'){
-                    count++;
-                    hamsters[i+1]='1';
-                }else if(i>0&&hamsters[i-1]=='.'){
-                    hamsters[i-1]='1';
-                    count++;
-                }else{
-                    return -1;
+        vector<int> arr;
+        int hamsterCount = 0;
+        int res = 0;
+
+        // Convert string to numeric vector
+        for (char c : hamsters) {
+            if (c == 'H') {
+                arr.push_back(1);
+                hamsterCount++;
+            } else {
+                arr.push_back(0);
+            }
+        }
+
+        int n = arr.size();
+
+        // First pass: handle 'H.H' pattern
+        for (int i = 1; i < n - 1; i++) {
+            if (arr[i] == 0 && arr[i - 1] == 1 && arr[i + 1] == 1) {
+                arr[i - 1] = arr[i] = arr[i + 1] = -1;  // mark as processed
+                res++;
+                hamsterCount -= 2;
+            }
+        }
+
+        // Second pass: handle remaining single Hs
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 1) {
+                if (i + 1 < n && arr[i + 1] == 0) {
+                    arr[i + 1] = -1;
+                    arr[i] = -1;
+                    res++;
+                    hamsterCount--;
+                } else if (i - 1 >= 0 && arr[i - 1] == 0) {
+                    arr[i - 1] = -1;
+                    arr[i] = -1;
+                    res++;
+                    hamsterCount--;
                 }
             }
-            
         }
-        
-        return count;
+
+        if (hamsterCount > 0) {
+            return -1;
+        }
+
+        return res;
     }
 };
