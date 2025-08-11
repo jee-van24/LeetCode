58c1660659b26be026f02c5948ee5aa9cb4dc377
Jeevan <jeevanv.professional@gmail.com>
2025-08-11 17:15:44 +0530

    Time: 0 ms (100%), Space: 10 MB (25.29%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -11,34 +11,12 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        int len=0;
-        ListNode* temp= head;
-        while(temp!=NULL){
-            len++;
-            temp=temp->next;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=nullptr&&fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        //now we got the length of the ll
-        if(len%2!=0){
-            //len is odd then return the l/2th node;
-            len=len/2;
-            int count=0;
-            ListNode* temp=head;
-            while(count!=len){
-                count++;
-                temp=temp->next;
-            }
-            head=temp;
-        }else{
-            //its even 
-            len=(len+1)/2;
-            int count=0;
-            ListNode* temp=head;
-            while(count!=len){
-                count++;
-                temp=temp->next;
-            }
-             head=temp;
-        }
-        return head;
+        return slow;
     }
 };
