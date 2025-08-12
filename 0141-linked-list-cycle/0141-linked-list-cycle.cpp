2cb1a8bdd0486fe6b44a2774a04e969fc75f0127
Jeevan <jeevanv.professional@gmail.com>
2025-08-13 02:08:43 +0530

    Time: 8 ms (80.83%), Space: 11.8 MB (53.09%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,17 +9,13 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* temp=head;
-        unordered_set<ListNode*>seen;
-        while(temp!=nullptr){
-            if(seen.find(temp)!=seen.end()){
-                return true;
-            }else{
-                seen.insert(temp);
-                temp=temp->next;
-            }
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=nullptr&&fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)return true;
         }
         return false;
-        
     }
 };
