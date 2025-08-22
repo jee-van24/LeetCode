10de1f9e74ca11d1dd14da07c0f90d5c9310712d
Jeevan <jeevanv.professional@gmail.com>
2025-08-22 16:17:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        if (x == 0) return 0;
+        if (x == 1) return 1;
+        if(x==-1&&n%2!=0)return -1;
+
+        long long exp = n;
+        if (exp < 0) exp = -exp;
+
+        double res = 1;
+        for (long long i = 0; i < exp; i++) {
+            res *= x;
+        }
+
+        return n >= 0 ? res : 1/res;
     }
 };
