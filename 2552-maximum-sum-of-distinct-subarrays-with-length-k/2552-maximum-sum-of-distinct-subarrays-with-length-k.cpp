20c6c4f33fb6fa6b1fbf4176db6a362849c1c9e3
Jeevan <jeevanv.professional@gmail.com>
2025-08-30 17:56:12 +0530

    Time: 135 ms (15.18%), Space: 99.5 MB (39.92%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,32 +1,39 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        if (k > n) return 0;
-
-        unordered_map<int,int> freq;
+        unordered_map<int,int> map;
         long long res = 0;
-        long long currsum = 0;
+        long long currsum = nums[0];
+        int left = 0;
+        map[nums[0]] = 1;
+        if (k == 1) return *max_element(nums.begin(), nums.end());
 
-        for (int i = 0; i < n; i++) {
-            currsum += nums[i];
-            freq[nums[i]] = freq[nums[i]] + 1;
-            if (i >= k) {
-                int out = nums[i - k];
-                freq[out] = freq[out] - 1;
-                if (freq[out] == 0) {
-                    freq.erase(out);
-                }
-                currsum -= out;
-            }
-
-            
-            if (i >= k - 1) {
-                if (freq.size() == k) {
-                    if (currsum > res) {
-                        res = currsum;
+        for (int right = 1; right < nums.size(); right++) {   // ✅ fix #1
+            if (map.find(nums[right]) == map.end()) {
+                map[nums[right]] = 1;
+                currsum += nums[right];
+            } else {
+                while (map.find(nums[right]) != map.end()) {
+                    currsum -= nums[left];
+                    map[nums[left]]--;
+                    if (map[nums[left]] == 0) {
+                        map.erase(nums[left]);
                     }
+                    left++;
                 }
+                map[nums[right]] = 1;
+                currsum += nums[right];
+            }
+            while (right - left + 1 > k) {
+                currsum -= nums[left];
+                map[nums[left]]--;
+                if (map[nums[left]] == 0) {
+                    map.erase(nums[left]);
+                }
+                left++;
+            }
+            if (right - left + 1 == k && map.size() == k) {
+                res = max(res, currsum);
             }
         }
         return res;
