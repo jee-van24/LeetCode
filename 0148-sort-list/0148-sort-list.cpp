ccf330a72ce7588ff5c1678ddf477f7dd3a348af
Jeevan <jeevanv.professional@gmail.com>
2025-09-01 02:09:37 +0530

    Time: 51 ms (29.61%), Space: 75.5 MB (48.75%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,28 +10,47 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        vector<int>nums;
-        ListNode* temp=head;
-        while(temp){
-            nums.push_back(temp->val);
+    ListNode* findmiddle(ListNode* head){
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        while(fast&&fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+    ListNode* merge2(ListNode* left,ListNode* right){
+        ListNode* dummy=new ListNode(-1);
+        ListNode* temp=dummy;
+        
+        while(left&&right){
+            if(left->val<right->val){
+                temp->next=left;
+                left=left->next;
+            }else{
+                temp->next=right;
+                right=right->next;
+            }
             temp=temp->next;
         }
-        sort(nums.begin(),nums.end());
-        ListNode* newhead=nullptr;
-        ListNode* newtail=nullptr;
-        for(auto n:nums){
-            ListNode* curr=new ListNode(n);
-            if(!newhead){
-                newhead=curr;
-                newtail=curr;   
-            }else{
-                newtail->next=curr;
-                newtail=curr;
-            }
+        if(left){
+            temp->next=left;
+        }else{
+            temp->next=right;
         }
-        return newhead;
+        return dummy->next;
+    }
+    ListNode* sortList(ListNode* head) {
+        if(head==nullptr||head->next==nullptr)return head;
+        ListNode* middle=findmiddle(head);
+        ListNode* right=middle->next;
+        middle->next=nullptr;
+        ListNode* left=head;
+        left=sortList(left);
+        right=sortList(right);
+        return merge2(left,right);
 
 
+        
     }
 };
