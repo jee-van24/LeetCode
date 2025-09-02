b33205c6078866e55c2f2a0936e300b5fe10eb02
Jeevan <jeevanv.professional@gmail.com>
2025-09-03 01:46:08 +0530

    Time: 3 ms (47.29%), Space: 76.9 MB (63.8%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -10,64 +10,37 @@
  */
 class Solution {
 public:
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr) {
+            ListNode* front = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = front;
+        }
+        return prev;
+    }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-
-        // Convert l1 -> string
-        string str1;
-        while (l1 != nullptr) {
-            str1.push_back('0' + l1->val);
-            l1 = l1->next;
-        }
-
-        // Convert l2 -> string
-        string str2;
-        while (l2 != nullptr) {
-            str2.push_back('0' + l2->val);
-            l2 = l2->next;
-        }
-
-        int i = str1.size() - 1;
-        int j = str2.size() - 1;
+        l1 = reverse(l1);
+        l2 = reverse(l2);
         int carry = 0;
-        stack<int> s;
-
-        // Step 1: add digit by digit from right to left
-        while (i >= 0 || j >= 0 || carry > 0) {
-            int d1 = 0;
-            if (i >= 0) {
-                d1 = str1[i] - '0';
-                i--;
-            }
-
-            int d2 = 0;
-            if (j >= 0) {
-                d2 = str2[j] - '0';
-                j--;
-            }
-
-            int sum = d1 + d2 + carry;
-            carry = sum / 10;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+        // now that 2 linkedlist are reversed
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int x = (l1 != nullptr) ? l1->val : 0;
+            int y = (l2 != nullptr) ? l2->val : 0;
+            int sum = x + y + carry;
             int digit = sum % 10;
-            s.push(digit);  // keep in reverse order
+            carry = sum / 10;
+            temp->next = new ListNode(digit);
+            temp = temp->next;
+            if (l1)
+                l1 = l1->next;
+            if (l2)
+                l2 = l2->next;
         }
-
-        // Step 2: build linked list from stack
-        ListNode* newhead = nullptr;
-        ListNode* tail = nullptr;
-
-        while (!s.empty()) {
-            ListNode* curr = new ListNode(s.top());
-            s.pop();
-
-            if (!newhead) {
-                newhead = curr;
-                tail = curr;
-            } else {
-                tail->next = curr;
-                tail = curr;
-            }
-        }
-
-        return newhead;
+        return reverse(dummy->next);
     }
 };
