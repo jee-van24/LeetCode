8af38cbf0400aac747957f85c40470a646b3b7e4
Jeevan <jeevanv.professional@gmail.com>
2025-09-13 18:00:31 +0530

    Time: 4 ms (19.67%), Space: 10.1 MB (15.36%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int maxFreqSum(string s) {
-        int maxv=0;
-        int maxc=0;
-        unordered_set<char>set={'a','e','i','o','u'};
-        unordered_map<char,int>map1;
-        unordered_map<char,int>map2;
-        for(auto ch:s){
-            if(set.find(ch)!=set.end()){
-                map1[ch]++;
-                maxv=max(maxv,map1[ch]);
-            }else{
-                map2[ch]++;
-                maxc=max(maxc,map2[ch]);
+        int maxv = 0;
+        int maxc = 0;
+        unordered_set<char> set = {'a', 'e', 'i', 'o', 'u'};
+        unordered_map<char, int> map;
+        for (auto ch : s) {
+            map[ch]++;
+            if (set.find(ch) != set.end()) {
+                maxv = max(maxv, map[ch]);
+            } else {
+                maxc = max(maxc, map[ch]);
             }
         }
 
-        return maxv+maxc;
+        return maxv + maxc;
     }
 };
