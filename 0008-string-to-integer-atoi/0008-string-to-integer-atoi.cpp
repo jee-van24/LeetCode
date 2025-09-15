33676153342424738f600d7a38a9fb9b216cf6cb
Jeevan <jeevanv.professional@gmail.com>
2025-09-16 00:18:09 +0530

    Time: 0 ms (100%), Space: 9.2 MB (23.29%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        long long res = 0;
-        int sign = 1;
-        int i = 0;
-
-        while (i < s.size() && s[i] == ' ') i++;
-
-        if (i < s.size() && (s[i] == '+' || s[i] == '-')) {
-            if (s[i] == '-') sign = -1;
+        int i=0;
+        while(i<s.size()&&s[i]==' '){
             i++;
         }
-
-        while (i < s.size() && isdigit(s[i])) {
-            int digit = s[i] - '0';
-
-            // Overflow check for 32-bit signed int range
-            if (res > (LLONG_MAX - digit) / 10) {
-                return sign == 1 ? INT_MAX : INT_MIN;
+        int res=0;
+        int sign=1;
+        if(i<s.size()&&(s[i]=='+'||s[i]=='-')){
+            if(s[i]=='-'){
+                sign=-1;
             }
-
-            res = res * 10 + digit;
             i++;
         }
-
-        res *= sign;
-        if (res > INT_MAX) return INT_MAX;
-        if (res < INT_MIN) return INT_MIN;
-
-        return static_cast<int>(res);
+        while(i<s.size()&&isdigit(s[i])){
+            int digit=s[i]-'0';
+            if(res>(INT_MAX-digit)/10){
+                return sign==1?INT_MAX:INT_MIN;
+            }
+            res=res*10+digit;
+            i++;
+        }
+        return sign*res;
     }
 };
