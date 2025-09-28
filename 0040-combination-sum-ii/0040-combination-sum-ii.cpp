84ccba6e8a74d387bfed814c2c80829d39c8caad
Jeevan <jeevanv.professional@gmail.com>
2025-09-28 21:54:31 +0530

    Time: 107 ms (5.01%), Space: 73.1 MB (5%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    set<vector<int>> seen;
-
     vector<vector<int>> dfs(vector<int> nums, int idx, vector<int> curr, int target) {
         vector<vector<int>> temp;
 
         if (target == 0) {
-            if (seen.find(curr) == seen.end()) {
-                temp.push_back(curr);
-                seen.insert(curr);
-            }
-            return temp;
+            temp.push_back(curr);
+            return temp;  
         }
 
-        if (idx >= nums.size() || target < 0) return temp;
+        if (idx >= nums.size() || target < 0)
+            return temp;
 
         for (int i = idx; i < nums.size(); i++) {
-            if (i > idx && nums[i] == nums[i-1]) continue; // skip duplicates
+            if (i > idx && nums[i] == nums[i - 1])
+                continue; 
 
-            vector<int> nextCurr = curr; 
-            nextCurr.push_back(nums[i]);
+            vector<int> nextCurr = curr;    
+            nextCurr.push_back(nums[i]);  
             auto left = dfs(nums, i + 1, nextCurr, target - nums[i]);
             temp.insert(temp.end(), left.begin(), left.end());
         }
@@ -28,9 +25,8 @@ public:
     }
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<int> curr;
-        sort(candidates.begin(), candidates.end()); // important for duplicates
-        dfs(candidates, 0, curr, target);
-        return vector<vector<int>>(seen.begin(), seen.end());
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>>res=dfs(candidates, 0, {}, target);
+        return res;
     }
 };
