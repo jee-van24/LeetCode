e14f8405555c68e5337efb5d89b87fabeaab2846
Jeevan <jeevanv.professional@gmail.com>
2025-09-28 13:29:50 +0530

    Time: 14 ms (8.87%), Space: 21.1 MB (6.3%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> backtrack(vector<int>& nums, vector<int> curr, int idx) {
+        if (idx == nums.size()) {
+            return {curr};
+        }
+
+        // include nums[idx]
+        curr.push_back(nums[idx]);
+        auto left = backtrack(nums, curr, idx + 1);
+
+        // exclude nums[idx]
+        curr.pop_back();
+        auto right = backtrack(nums, curr, idx + 1);
+
+        // merge both results
+        left.insert(left.end(), right.begin(), right.end());
+        return left;
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        // generate all subsets
+        vector<vector<int>> res = backtrack(nums, {}, 0);
+
+        // use set to remove duplicates
+        set<vector<int>> s(res.begin(), res.end());
+
+        // return as vector
+        return vector<vector<int>>(s.begin(), s.end());
+    }
+};
