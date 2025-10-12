45fdb77603ac836415c63e742959ca117663c992
Jeevan <jeevanv.professional@gmail.com>
2025-10-13 01:28:40 +0530

    Time: 0 ms (100%), Space: 10.1 MB (74.81%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
-    vector<vector<string>> res;
-
-    vector<vector<string>> solveNQueens(int n) {
-        vector<string> board(n, string(n, '.'));
-        backtrack(board, 0);
-        return res;
-    }
-
-    void backtrack(vector<string>& board, int row) {
-        if (row == board.size()) {
+    void dfs(int col,vector<string>&board,vector<vector<string>>&res,vector<bool>&rowused,vector<bool>&upperdiagused,vector<bool>&lowerdiagused,int n){
+        if(col==n){
             res.push_back(board);
             return;
         }
-        for (int col = 0; col < board.size(); col++) {
-            if (isSafe(board, row, col)) {
-                board[row][col] = 'Q';
-                backtrack(board, row + 1);
-                board[row][col] = '.';
+        //now place the queen at the curr column in any of the 0 to n-1 rows available 
+        for(int row=0;row<n;row++){
+            if(rowused[row]==false&&lowerdiagused[row+col]==false&&upperdiagused[n-1+row-col]==false){
+                board[row][col]='Q';
+                rowused[row]=true;
+                lowerdiagused[row+col]=true;
+                upperdiagused[n-1+row-col]=true;
+                dfs(col+1,board,res,rowused,upperdiagused,lowerdiagused,n);
+                //backtrack for the next other possibilities 
+                board[row][col]='.';
+                rowused[row]=false;
+                lowerdiagused[row+col]=false;
+                upperdiagused[n-1+row-col]=false;  
             }
         }
     }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>res;
+        vector<string>board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            board[i]=s;
+        }
+        vector<bool>upperdiag(2*n-1,false),lowerdiag(2*n-1,false),row(n,false);
+        dfs(0,board,res,row,upperdiag,lowerdiag,n);
+        return res;
 
-    bool isSafe(vector<string>& board, int row, int col) {
-        for (int i = 0; i < row; i++) {
-            if (board[i][col] == 'Q') return false;
-        }
-        for (int i = 1; i <= min(row, col); i++) {
-            if (board[row - i][col - i] == 'Q') return false;
-        }
-        for (int i = 1; i <= min(row, (int)board.size() - 1 - col); i++) {
-            if (board[row - i][col + i] == 'Q') return false;
-        }
-        return true;
     }
 };
