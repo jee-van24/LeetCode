066a6e886d57a9e28b25f21bda27f705140fe05a
Jeevan <jeevanv.professional@gmail.com>
2025-10-13 22:15:14 +0530

    Time: 5 ms (24.28%), Space: 21.7 MB (6.94%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     vector<string> removeAnagrams(vector<string>& words) {
         vector<string>res;
+        vector<unordered_map<char,int>>freq(words.size());
+        for(int i=0;i<words.size();i++){
+            for(auto ch: words[i]){
+                freq[i][ch]++;
+            }
+        }
         res.push_back(words[0]);
-        string prev=words[0];
-        sort(prev.begin(),prev.end());
-        //for now the map has the frequency of chars of the first word
         for(int i=1;i<words.size();i++){
-            auto currword=words[i];
-            auto sortedcurrword=currword;
-            sort(sortedcurrword.begin(),sortedcurrword.end());
-            if(sortedcurrword!=prev){
-                // it is not a anagram 
-                //add this to the result and make it the last prev
-                res.push_back(currword);
-                prev=sortedcurrword;
-
+            if(freq[i]!=freq[i-1]){
+                res.push_back(words[i]);
             }
         }
         return res;
-
     }
 };
