65cf3146f527998699cc7f8070a3b5e741579471
Jeevan <jeevanv.professional@gmail.com>
2025-10-14 20:00:41 +0530

    Time: 7 ms (94.69%), Space: 40.5 MB (29.65%) - LeetHub

--- 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.cpp
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool hasIncreasingSubarrays(vector<int>& nums, int k) {
-        int knew = k-1;
-        if(knew == 0)return true;
+        int needed= k-1;
+        if(needed == 0)return true;
         for(int j=k+1;j<nums.size();j++){
-            if(nums[j] > nums[j-1] && nums[j-k] > nums[j-k-1])knew--;
-            else knew = k - 1;
-            if(knew == 0)return true;
+            if(nums[j] > nums[j-1] && nums[j-k] > nums[j-k-1])needed--;
+            else needed = k - 1;
+            if(needed == 0)return true;
         }
         return false;
     }
