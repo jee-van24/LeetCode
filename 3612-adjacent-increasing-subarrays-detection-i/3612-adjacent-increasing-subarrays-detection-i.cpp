fc6ba6c3658c47e8ab853b7a336e659955d6d674
Jeevan <jeevanv.professional@gmail.com>
2025-10-15 20:06:56 +0530

    Time: 19 ms (21.36%), Space: 40.9 MB (23.18%) - LeetHub

--- 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.cpp
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.cpp
@@ -1,12 +1,33 @@
 class Solution {
 public:
     bool hasIncreasingSubarrays(vector<int>& nums, int k) {
-        int needed= k-1;
-        if(needed == 0)return true;
-        for(int j=k+1;j<nums.size();j++){
-            if(nums[j] > nums[j-1] && nums[j-k] > nums[j-k-1])needed--;
-            else needed = k - 1;
-            if(needed == 0)return true;
+        vector<int>lens;
+        if(k==1){
+            return true;
+        }
+        int currlen=1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>nums[i-1]){
+                currlen++;
+            }else{
+                lens.push_back(currlen);
+                currlen=1;
+            }
+        }
+        lens.push_back(currlen);
+        for(auto len:lens){
+            cout<<len<<" ";
+        }
+        if(lens.size()==1&&lens[0]>=2*k){
+            return true;
+        }
+        for(int i=0;i<lens.size()-1;i++){
+            if(lens[i]>=2*k||lens[i]>=k&&lens[i+1]>=k){
+                return true;
+            }
+        }
+        if(!lens.empty()&&lens[lens.size()-1]>=2*k){
+            return true;
         }
         return false;
     }
