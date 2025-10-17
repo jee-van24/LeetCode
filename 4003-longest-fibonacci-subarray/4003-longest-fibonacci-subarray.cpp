112687d7f44994a017da18afb470028fd927b11b
Jeevan <jeevanv.professional@gmail.com>
2025-10-17 14:45:32 +0530

    Time: 0 ms (100%), Space: 102.8 MB (19.74%) - LeetHub

---
+++ 4003-longest-fibonacci-subarray/4003-longest-fibonacci-subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+       
+       int res=2;
+       int currlen=0;
+       for(int right=2;right<nums.size();right++){
+            if(nums[right-2]+nums[right-1]==nums[right]){
+                if(currlen==0){
+                    currlen=3;
+                }else{
+                    currlen++;
+                }
+            }else{
+                currlen=2;
+            }
+            res=max(res,currlen);
+       } 
+       return res;
+    }
+};
