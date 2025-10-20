1a32ee25e37782b072653fc4b7e9818b9171d6bc
Jeevan <jeevanv.professional@gmail.com>
2025-10-20 09:33:04 +0530

    Time: 0 ms (100%), Space: 18 MB (10.37%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.cpp
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.cpp
@@ -2,13 +2,22 @@ class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
         int res = 0;
-        for(auto str:operations){
-            if(str=="X++"||str=="++X"){
-                res++;
+        unordered_map<string, int> map{
+            {"++X", 0}, 
+            {"X++", 0},
+            {"--X",0},
+            {"X--",0}
+        };
+        for (auto str : operations) {
+            map[str]++;
+        }
+        for (auto &p :map) {
+            if(p.first=="X++"||p.first=="++X"){
+                res += p.second;
             }else{
-                res--;
-            }
-        }    
+                res-=p.second;
+            }   
+        }
         return res;
     }
 };
