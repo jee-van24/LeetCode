4c7be5605a50e3f6f16e7313a4a20cb9a52e5f16
Jeevan <jeevanv.professional@gmail.com>
2025-10-28 11:50:45 +0530

    Time: 14 ms (18.53%), Space: 33 MB (27.8%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        unordered_map<string,pair<int,int>>map;
-        for (int i = 0; i < bank.size(); i++) {
-            int cnt = 0;
-            for (char ch : bank[i]) {
-                if (ch == '1') cnt++;
-            }
-            map[to_string(i) + bank[i]]={i,cnt};
-        }
+        int prev=-1;
         int res=0;
-        int left=0;
-        for(int right=1;right<bank.size();right++){
-            int prevcnt=map[to_string(left)+bank[left]].second;
-            int currcnt=map[to_string(right)+bank[right]].second;
-            if(prevcnt!=0&&currcnt!=0){
-                res+=prevcnt*currcnt;
-                left=right;
-            }else if(prevcnt!=0&&currcnt==0){
-                continue;
-            }else if(prevcnt==0&&currcnt!=0){
-                left=right;
+        for(auto row:bank){
+            int curr=0;
+            for(auto device:row){
+                if(device=='1'){
+                    curr++;
+                }
+            }
+            if(curr!=0){
+                if(prev!=-1){
+                    res+=prev*curr;
+                }
+                prev=curr;
             }
         }
+
         cout<<res;
         return res;
     }
