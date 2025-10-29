a838d519b9525c6ebe7302f7a9052362cbb57f8f
Jeevan <jeevanv.professional@gmail.com>
2025-10-29 13:58:51 +0530

    Time: 0 ms (100%), Space: 8.5 MB (11.95%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     int smallestNumber(int n) {
-        string binary=bitset<32>(n).to_string();
-        int len=binary.size();
-        string check="";
-        for(int i=0;i<32;i++){
-            check.push_back('1');
-            int decimal = stoi(check, nullptr, 2);
-            if(decimal>=n){
-                return decimal;
-            }
-        }
-        return 0;
+        int bits = floor(log2(n)) + 1;
+        int maxi=(1<<bits)-1;
+        int comp1=n&maxi;
+        return n|maxi;
     }
 };
