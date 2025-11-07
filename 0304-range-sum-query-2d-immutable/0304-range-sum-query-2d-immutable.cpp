9481959917848513a085cc13c23197be5015f117
Jeevan <jeevanv.professional@gmail.com>
2025-11-07 17:31:34 +0530

    Time: 36 ms (23.69%), Space: 157.1 MB (5.69%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -1,28 +1,29 @@
 class NumMatrix {
 public:
     vector<vector<int>>prefix;
-    NumMatrix(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        prefix=vector<vector<int>>(m,vector<int>(n,0));
-        for(int i=0;i<m;i++){
-            prefix[i][0]=matrix[i][0];
-            for(int j=1;j<n;j++){
-                prefix[i][j]=matrix[i][j]+prefix[i][j-1];
+    NumMatrix(vector<vector<int>>& matrix) { 
+        for(int i=0;i<matrix.size();i++){
+            vector<int>temp;
+            temp.push_back(matrix[i][0]);
+            for(int j=1;j<matrix[0].size();j++){
+                temp.push_back(matrix[i][j]+temp[j-1]);
             }
-        } 
+            prefix.push_back(temp);
+        }
+        
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        int res=0;
+        int sum=0;
+        int l1=col1,l2=col2;
         for(int i=row1;i<=row2;i++){
             if(col1==0){
-                res+=prefix[i][col2];
+                sum+=prefix[i][l2];
             }else{
-                res+=prefix[i][col2]-prefix[i][col1-1];
+                sum+=prefix[i][l2]-prefix[i][col1-1];
             }
         }
-        return res;
+        return sum;
     }
 };
 
