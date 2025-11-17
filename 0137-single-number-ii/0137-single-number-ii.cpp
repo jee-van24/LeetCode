6e852c587431752706a208f5a5e8b231e2ed9348
Jeevan <jeevanv.professional@gmail.com>
2025-11-18 00:45:38 +0530

    Time: 2 ms (38.96%), Space: 13.5 MB (39.89%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int>map;
-        for(int n:nums){
-            map[n]++;
-        }
-        for(auto &p:map){
-            if(p.second==1){
-                return p.first;
+        int res=0;
+        for(int bitidx=0;bitidx<32;bitidx++){
+            int cnt=0;
+            for(int i=0;i<nums.size();i++){
+                if((nums[i]&(1<<bitidx))){
+                    cnt++;
+                }
+            }
+            if(cnt%3==1){
+                res=res|(1<<bitidx);
             }
         }
-        return 0;
+        return res;
     }
 };
