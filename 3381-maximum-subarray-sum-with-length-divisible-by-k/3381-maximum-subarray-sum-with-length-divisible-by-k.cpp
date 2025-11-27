cfbc523191218bedb367b5bbe2a7e2a105d5b7f6
Jeevan <jeevanv.professional@gmail.com>
2025-11-28 02:42:53 +0530

    Time: 11 ms (65.77%), Space: 171.4 MB (65.77%) - LeetHub

--- 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
+++ 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-
-    // for each offset <= k
-    // do kadanes with elements of group size k combined
-
     long long maxSubarraySum(vector<int>& nums, int k) {
-
-        int n = nums.size();
-
-        vector<long long> pref(n + 1);
-        for (int i = 1; i <= n; ++i) pref[i] = pref[i - 1] + nums[i - 1];
-
-        long long res = LLONG_MIN;
-
-        for (int off = 0; off < k; ++off) {
-            long long cur = 0;
-            for (int i = off; i + k <= n; i += k) {
-                cur += pref[i + k] - pref[i];
-                res = max(res, cur);
-                cur = max(cur, 0LL);
+        int n=nums.size();
+        vector<long long>prefix(n);
+        prefix[0]=nums[0];
+        for(int i=1;i<nums.size();i++){
+            prefix[i]=nums[i]+prefix[i-1];
+        }
+        long long res=LLONG_MIN;
+        for(int i=0;i<k;i++){
+            long long currsum=0;
+            for(int j=i;j+k-1<nums.size();j+=k){
+                long long ksum=prefix[j+k-1]-(j==0?0:prefix[j-1]);
+                currsum+=ksum;
+                res=max(res,currsum);
+                currsum=max(currsum,0ll);
             }
         }
-
         return res;
-        
     }
 };
