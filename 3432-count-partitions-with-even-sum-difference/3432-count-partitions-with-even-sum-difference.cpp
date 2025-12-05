118d2e17ba10bf615316bed9080757a3ba18ad0a
Jeevan <jeevanv.professional@gmail.com>
2025-12-05 12:24:58 +0530

    Time: 0 ms (100%), Space: 22.5 MB (53.79%) - LeetHub

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
@@ -4,7 +4,7 @@ public:
         int res = 0;
         int currsum = 0;
         int totsum = accumulate(nums.begin(), nums.end(), 0);
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size()-1; i++) {
             currsum += nums[i];
             int remsum = totsum - currsum;
             if ((remsum - currsum) % 2 == 0) {
@@ -12,6 +12,6 @@ public:
             }
         }
         if(res==0)return res;
-        return res-1;
+        return res;
     }
 };
