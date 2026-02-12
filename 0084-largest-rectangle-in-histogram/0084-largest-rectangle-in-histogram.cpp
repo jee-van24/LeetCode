1748cc8e79ef12c7b3855144d019021f81e8b57f
Jeevan <jeevanv.professional@gmail.com>
2026-02-13 00:29:42 +0530

    Time: 2867 ms (5.15%), Space: 161.2 MB (5.18%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,25 +1,51 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        vector<pair<int, int>> stack;
-        int ret = 0;
-        heights.push_back(0);
-        for (int i = 0; i < heights.size(); i++) {
-            int left = i;
-            if (!stack.empty()) {
-                int height = stack.back().first;
-                while (!stack.empty() && stack.back().first >= heights[i]) {
-                    height = min(height, stack.back().first);
-                    left = min(left, stack.back().second);
-                    ret = max(ret, height * (i - 1 - left + 1));
-                    stack.pop_back();
-                }
-
-                ret = max(ret, heights[i] * (i - left + 1));
+        stack<vector<int>>s;
+        int n=heights.size();
+        vector<int>prev(n),next(n);
+        
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty()&&s.top()[1]>heights[i]){
+                s.pop();
             }
-            stack.emplace_back(heights[i], left);
+            if(!s.empty()){
+                next[i]=s.top()[0];
+            }else{
+                next[i]=n;
+            }
+            s.push({i,heights[i]});
         }
-
-        return ret;
+        cout<<"next: ";
+        for(auto n:next){
+            cout<<n<<" ";
+        }
+        cout<<endl;
+        while(!s.empty()){
+            s.pop();
+        }
+        for(int i=0;i<n;i++){
+            while(!s.empty()&&s.top()[1]>=heights[i]){
+                s.pop();
+            }
+            if(!s.empty()){
+                prev[i]=s.top()[0];
+            }else{
+                prev[i]=-1;
+            }
+            s.push({i,heights[i]});
+        }
+        cout<<"prev :";
+        for(auto n:prev){
+            cout<<n<<" ";
+        }
+        int res=0;
+        for(int i=0;i<n;i++){
+            int left=prev[i]+1;
+            int right=next[i]-1;
+            int len=right-left+1;
+            res=max(res,heights[i]*len);
+        }
+        return res;
     }
 };
