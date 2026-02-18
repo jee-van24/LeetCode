820e221461ed90036cd91821eb35e27578a24a7f
Jeevan <jeevanv.professional@gmail.com>
2026-02-19 01:35:23 +0530

    Time: 0 ms (100%), Space: 7.8 MB (31.1%) - LeetHub

---
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string str(int n){
+        string res;
+        while(n!=0){
+            char rem=(n%2)+'0';
+            res.push_back(rem);
+            n/=2;
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+    bool hasAlternatingBits(int n) {
+        string res=str(n);
+        for(int i=1;i<res.size();i++){
+            if(res[i]==res[i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
