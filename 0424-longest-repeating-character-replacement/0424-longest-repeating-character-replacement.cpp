4ed234993f8d420d7f196aaff3176c3d68a3d110
Jeevan <jeevanv.professional@gmail.com>
2026-02-23 00:52:57 +0530

    Time: 12 ms (23.05%), Space: 11.3 MB (5.12%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,18 +2,22 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int res=0;
+        unordered_map<char,int>map;
         int left=0;
         int maxfreq=0;
-        unordered_map<char,int>map;
         for(int right=0;right<s.size();right++){
             map[s[right]]++;
             maxfreq=max(maxfreq,map[s[right]]);
-            while(right-left+1-maxfreq>k){
+            while((right-left+1)-maxfreq>k){
                 map[s[left]]--;
+               
+                if(map[s[left]]==0){
+                    map.erase(s[left]);
+                }
                 left++;
             }
-            res=max(right-left+1,res);
+            res=max(res,right-left+1);
         }
         return res;
-    }
+    }   
 };
