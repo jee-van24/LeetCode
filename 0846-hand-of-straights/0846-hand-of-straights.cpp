b9bad60bd656915c55d9a6bb976e69af4d43b906
Jeevan <jeevanv.professional@gmail.com>
2026-03-13 20:04:47 +0530

    Time: 32 ms (76.98%), Space: 33.9 MB (9.5%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -2,24 +2,27 @@ class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
         sort(begin(hand),end(hand));
-        unordered_set<int>s;
+        map<int,int>map;
         if(hand.size()%groupSize!=0)return false;
-        for(int i=0;i<hand.size();i++){
-            //see if the current idx can be a starting point
-            if(!s.count(i)){
-                s.insert(i);
-                int cnt=1;
-                int curr=hand[i];
-                int j=i+1;
-                while(j<hand.size()&&cnt!=groupSize){
-                    if(!s.count(j)&&hand[j]==curr+1){
-                        s.insert(j);
-                        curr=hand[j];
-                        cnt++;
-                    }
-                    j++;
+        for(auto n:hand)map[n]++;
+        while(true){
+            if(map.empty())break;
+            int prev=map.begin()->first;
+            vector<int>temp;
+            temp.push_back(prev);
+            for(auto &p:map){
+                if(temp.size()==groupSize)break;
+                if(p.first==prev+1){
+                    temp.push_back(p.first);
+                    prev+=1;
                 }
-                if(cnt!=groupSize)return false;
+            }
+            if(temp.size()!=groupSize)return false;
+            for(int i=0;i<temp.size();i++){
+                map[temp[i]]--;
+                if(map[temp[i]]==0){
+                    map.erase(temp[i]);
+                }
             }
         }
         return true;
