ebae20e01ac5633cf7fbe5e79882820a77cb9465
Jeevan <jeevanv.professional@gmail.com>
2026-05-30 14:35:33 +0530

    Time: 9 ms (76.91%), Space: 19.1 MB (28.32%) - LeetHub

---
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int solve(int idx, vector<int>&arr,vector<int>&dp,int d){
+        if(dp[idx]!=-1){
+            //already reached this idx and i have the best answer stored already so dont recompute the same state
+            return dp[idx];
+        }
+        int temp1=0,temp2=0;
+        for(int j=idx+1;j<=min((int)arr.size()-1,idx+d);j++){
+            if(arr[j]>=arr[idx])break;
+            //its a valid jump, call the next recursive call
+            temp1=max(temp1,1+solve(j,arr,dp,d));
+        }
+        for(int j=idx-1;j>=max(0,idx-d);j--){
+            if(arr[j]>=arr[idx])break;
+            temp2=max(temp2,1+solve(j,arr,dp,d));
+        }
+        return dp[idx]=max(temp1,temp2);
+    }
+    int maxJumps(vector<int>& arr, int d) {
+        int n=arr.size();
+        vector<int>dp(n,-1);
+        int res=0;
+        for(int i=0;i<n;i++){
+           res=max(res,solve(i,arr,dp,d));
+        }
+        return res+1;
+    }
+};
