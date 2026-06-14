07b820e6c030d1dbd4f8720fc8ce5462738e92ac
Jeevan <jeevanv.professional@gmail.com>
2026-06-14 23:58:17 +0530

    Time: 3 ms (66.81%), Space: 138.4 MB (10.5%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -11,22 +11,16 @@
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        unordered_map<int, ListNode*>map;
-        int i=0;
-        int len=0;
-        int res=0;
-        for(ListNode* curr=head;curr!=nullptr;curr=curr->next){
-            map[i]=curr;
-            i++;
-            len++;
+        vector<int>arr;
+        ListNode* curr=head;
+        while(curr){
+            arr.push_back(curr->val);
+            curr=curr->next;
         }
-        cout<<len;
-        for(int i=0;i<=len/2-1;i++){
-            ListNode* temp1=map[i];
-            ListNode* temp2=map[len-i-1];
-            int s1=temp1->val;
-            int s2=temp2->val;
-            res=max(res,s1+s2);
+        int res=0;
+        for(int i=0;i<arr.size()/2;i++){
+            int sum=arr[i]+arr[arr.size()-i-1];
+            res=max(res,sum);
         }
         return res;
     }
