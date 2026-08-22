61caf0ce4c24924864697c19822b5c22572a1fec
Jeevan <jeevanv.professional@gmail.com>
2026-08-22 14:56:06 +0530

    Time: 24 ms (25%), Space: 30.4 MB (59%) - LeetHub

--- 3980-minimum-operations-to-transform-binary-string/3980-minimum-operations-to-transform-binary-string.cpp
+++ 3980-minimum-operations-to-transform-binary-string/3980-minimum-operations-to-transform-binary-string.cpp
@@ -1,39 +1,45 @@
 class Solution {
 public:
     int minOperations(string s1, string s2) {
-        int res=0;
-        if(s1.size()==1){
-            if(s1[0]==s2[0]){
+        int res = 0;
+
+        if (s1.size() == 1) {
+            if (s1[0] == s2[0]) {
                 return 0;
-            }else if(s1[0]=='0'&&s2[0]=='0'){
+            } else if (s1[0] == '0' && s2[0] == '0') {
                 return 1;
-            }else if(s1[0]=='0'){
+            } else if (s1[0] == '0') {
                 return 1;
-            }else{
+            } else {
                 return -1;
             }
-                
         }
-        for(int i=0;i<s1.size();i++){
-            if(s1[i]==s2[i]){
+
+        for (int i = s1.size() - 1; i >= 0; i--) {
+            if (s1[i] == s2[i]) {
                 continue;
-            }else{
-                if(s1[i]=='0'&&s2[i]=='1'){
-                    s1[i]='1';
+            } else {
+                if (s1[i] == '0' && s2[i] == '1') {
+                    s1[i] = '1';
                     res++;
-                }else if(s1[i]=='1'&&s2[i]=='0'){
-                    if(i+1<s1.size()&&s1[i+1]=='1'){
-                        res+=1;
-                        s1[i+1]='0';
-                    }else if(s1[i+1]=='0'){
-                        res+=2;
-                        s1[i+1]='0';
-                    }else if(i-1>=0){
-                        res+=2;
+                }
+                else if (s1[i] == '1' && s2[i] == '0') {
+
+                    if (i - 1 >= 0 && s1[i - 1] == '1') {
+                        res += 1;
+                        s1[i - 1] = '0';
+                    }
+                    else if (i - 1 >= 0 && s1[i - 1] == '0') {
+                        res += 2;
+                        s1[i - 1] = '0';
+                    }
+                    else if (i + 1 < s1.size()) {
+                        res += 2;
                     }
                 }
             }
         }
-         return res;
+
+        return res;
     }
 };
