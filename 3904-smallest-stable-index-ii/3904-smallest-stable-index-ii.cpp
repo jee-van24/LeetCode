e6b63c0c0a66a2c5ae468ede6db45da6c8544506
Jeevan <jeevanv.professional@gmail.com>
2026-09-05 11:08:38 +0530

    Time: 11 ms (57.63%), Space: 210.9 MB (35.81%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> pmax(n, 0), pmin(n, 0);
+        pmax[0] = nums[0];
+        pmin[n - 1] = nums[n - 1];
+        for (int i = 1; i < n; i++) {
+            pmax[i] = max(pmax[i - 1], nums[i]);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            pmin[i] = min(pmin[i + 1], nums[i]);
+        }
+
+        int minscore = INT_MAX;
+        int res = -1;
+        for (int i = 0; i < n; i++) {
+            int maxval = pmax[i];
+            int minval = pmin[i];
+            int score = maxval - minval;
+            if (score <= k)
+                return i;
+        }
+        return -1;
+    }
+};
