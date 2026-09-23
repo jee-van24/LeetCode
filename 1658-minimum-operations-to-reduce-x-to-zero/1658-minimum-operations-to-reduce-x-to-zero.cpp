fa7725ac40ae6235d4aa11b038fa49ff40acd60f
Jeevan <jeevanv.professional@gmail.com>
2026-09-23 19:18:29 +0530

    Time: 211 ms (11.86%), Space: 176.9 MB (5.19%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,28 +1,58 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        int s=0;
-        int targ;
-        for(auto n:nums){
-            s+=n;
-        }
-        targ=s-x;
-        if(targ<0)return -1;
-        if(targ==0)return nums.size();
-        int l=0;
-        int cs=0;
-        int n=nums.size(),res=INT_MAX;
-        for(int r=0;r<nums.size();r++){
-            cs+=nums[r];
-            while(cs>targ){
-                cs-=nums[l];
-                l+=1;
-            }
-            if(cs==targ){
-                res=min(res,n-(r-l+1));
-            }
-        }
-        return res==INT_MAX?-1:res;
-    }
 
+        int n = nums.size();
+
+        vector<int> pleft(n, 0);
+
+        pleft[0] = nums[0];
+
+        for(int i = 1; i < n; i++) {
+            pleft[i] = pleft[i - 1] + nums[i];
+        }
+
+        unordered_map<int, int> suffix;
+
+        int sum = 0;
+
+        for(int i = n - 1; i >= 0; i--) {
+
+            sum += nums[i];
+
+            int rightCount = n - i;
+
+            suffix[sum] = rightCount;
+        }
+
+        int res = INT_MAX;
+
+        for(int leftCount = 0; leftCount <= n; leftCount++) {
+
+            int leftSum = 0;
+
+            if(leftCount > 0)
+                leftSum = pleft[leftCount - 1];
+
+            if(leftSum > x)
+                break;
+
+            int needed = x - leftSum;
+
+            if(needed == 0) {
+                res = min(res, leftCount);
+                continue;
+            }
+            if(suffix.count(needed)) {
+
+                int rightCount = suffix[needed];
+
+                if(leftCount + rightCount <= n) {
+                    res = min(res, leftCount + rightCount);
+                }
+            }
+        }
+
+        return res == INT_MAX ? -1 : res;
+    }
 };
